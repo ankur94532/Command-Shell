e218f8f93b12eb95a5f3e80ceb89b536f48e3bc4
Shashwat Tripathi <shashwattrpth96@gmail.com>
2025-09-19 21:08:18 +0530

    hi

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,12 +4,11 @@ public class Main {
     public static void main(String[] args) throws Exception {
         // Uncomment this block to pass the first stage
         System.out.print("$ ");
+        Scanner scanner = new Scanner(System.in);
         while (true) {
-            Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
             System.out.println(input + ": command not found");
             System.out.print("$ ");
-            scanner.close();
         }
     }
 }
