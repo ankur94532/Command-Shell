432632a896666a9f40b67e746ce4935ef8f9f601
Shashwat Tripathi <shashwattrpth96@gmail.com>
2025-09-19 20:58:24 +0530

    hi

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        System.out.print("$ ");
         while (true) {
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
