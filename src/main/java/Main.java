fd561c30d3e4492b120b22a977db4dcd67225dcc
Shashwat Tripathi <shashwattrpth96@gmail.com>
2025-09-19 21:27:01 +0530

    hi

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,8 +7,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String input = scanner.nextLine();
-            System.err.println(input.substring(0, 4));
-            if (input.substring(0, 3) == "echo") {
+            if (input.substring(0, 4) == "echo") {
                 System.out.println(input.substring(5));
             } else {
                 System.out.println(input + ": command not found");
