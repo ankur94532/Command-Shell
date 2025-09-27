a5c064c1623de7e8b56e1ed825f6789f01b5aeac
Shashwat Tripathi <shashwattrpth96@gmail.com>
2025-09-27 21:44:35 +0530

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -155,9 +155,7 @@ public class Main {
             int r = in.read();
             char c = (char) (r);
             if (c == '\n' || c == '\r') { // Enter
-                System.out.print("\r\033[2K");
-                System.out.print("$ ");
-                System.out.println(sb.toString());
+                System.out.print("\r\n");
                 System.out.flush();
                 return sb.toString();
             } else if (c == '\t') {
